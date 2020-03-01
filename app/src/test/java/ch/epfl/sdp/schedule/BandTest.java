d6727c01d6a578ed5da73e3bde4ac195afb85e92
Axel Marmet <axel.marmet@epfl.ch>
2020-03-01 21:13:28 +0100

    Add BandTest.java

--- app/src/test/java/ch/epfl/sdp/schedule/BandTest.java
+++ app/src/test/java/ch/epfl/sdp/schedule/BandTest.java
@@ -1,4 +1,66 @@
 package ch.epfl.sdp.schedule;
 
+import android.graphics.Bitmap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static org.junit.Assert.*;
+
+
 public class BandTest {
+
+    String name;
+    Optional<String> description;
+    Optional<Bitmap> picture;
+
+    @Before
+    public void setup(){
+        name = "My band";
+        description = Optional.empty();
+        picture = Optional.empty();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructorFailsWhenNameIsNull(){
+        Band test = new Band(null, description, picture);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructorFailsWhenDescriptionIsNull(){
+        Band test = new Band(name, null, picture);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructorFailsWhenPictureIsNull(){
+        Band test = new Band(name, description, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorFailsWhenNameIsEmpty(){
+        Band test = new Band("", description, picture);
+    }
+
+    @Test
+    public void getNameReturnsExpectedName(){
+        Band test = new Band(name, description, picture);
+        assertEquals(name, test.getName());
+    }
+
+    @Test
+    public void getDescriptionReturnsExpectedDescription(){
+        Band test = new Band(name, description, picture);
+        assertEquals(description, test.getDescription());
+    }
+
+    @Test
+    public void getPictureReturnsExpectedPicture(){
+        Band test = new Band(name, description, picture);
+        assertEquals(picture, test.getPicture());
+    }
+
+
 }
