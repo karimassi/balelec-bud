2fc3ca077c8da816e6b5d23b4999630a3fdf8483
celinecamacho <celine.camacho@epfl.ch>
2020-03-03 19:41:16 +0100

    Implemented the Map View

--- app/src/main/java/ch/epfl/balelecbud/MainActivity.java
+++ app/src/main/java/ch/epfl/balelecbud/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapActivity();
+            }
+        });
+    }
+
+    /** Called when the user taps the Map button */
+    public void openMapActivity () {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 
     /** Called when the user taps the Send button */
---
+++ app/src/main/java/ch/epfl/balelecbud/MapActivity.java
@@ -0,0 +1,32 @@
+package ch.epfl.balelecbud;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng currentPosition = new LatLng(46.518802,6.567550);
+        map.addMarker(new MarkerOptions().position(currentPosition).title("currentPosition"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+    }
+}
