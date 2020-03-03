d4fee1b28414ae52a2961e58f2b19ddb6df28b36
Axel Marmet <axel.marmet@epfl.ch>
2020-03-03 09:49:10 +0100

    Update Preconditions.java

--- app/src/main/java/ch/epfl/sdp/utils/Preconditions.java
+++ app/src/main/java/ch/epfl/sdp/utils/Preconditions.java
@@ -12,4 +12,10 @@ public class Preconditions {
             throw new IllegalArgumentException(message);
         }
     }
+
+    public static void checkNonNull(Object obj){
+        if(obj == null){
+            throw new NullPointerException();
+        }
+    }
 }
