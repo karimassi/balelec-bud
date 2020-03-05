6f277308319436d1cb6cb014b84ef63cc52a4299
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-03-05 19:29:27 +0100

    Some cleanup and tests

--- app/src/main/java/ch/epfl/balelecbud/MainActivity.java
+++ app/src/main/java/ch/epfl/balelecbud/MainActivity.java
@@ -18,8 +18,6 @@ import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import ch.epfl.balelecbud.localization.LocalizationService;
 
@@ -33,42 +31,32 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
-    /**
-     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
-     */
-    private static final long UPDATE_INTERVAL = 6_000; // Every 60 seconds.
-
-    /**
-     * The fastest rate for active location updates. Updates will never be more frequent
-     * than this value, but they may be less frequent.
-     */
-    private static final long FASTEST_UPDATE_INTERVAL = 3_000; // Every 30 seconds
-
-    /**
-     * The max time before batched results are delivered by location services. Results may be
-     * delivered sooner than this interval.
-     */
+    private static final long UPDATE_INTERVAL = 6_000;
+    private static final long FASTEST_UPDATE_INTERVAL = 3_000;
     private static final long MAX_WAIT_TIME = UPDATE_INTERVAL; // Every times a value is ready
 
-    /**
-     * Stores parameters for requests to the FusedLocationProviderApi.
-     */
     private LocationRequest lr;
-
-    /**
-     * Provides access to the Fused Location Provider API.
-     */
     private FusedLocationProviderClient client;
+
+    public boolean isLocalizationActive() {
+        return isLocalizationActive;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //LocalizationService.getLocalizationService(this);
         this.isLocalizationActive = false;
 
+        // Check if the user revoked runtime permissions.
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            this.ls.setClickable(true);
+        }
 
         this.ls = findViewById(R.id.localizationSwitch);
+
         this.ls.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -83,123 +71,29 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
             }
         });
 
-        // Check if the user revoked runtime permissions.
-        if (!checkPermissions()) {
-            requestPermissions();
-        }
 
         client = LocationServices.getFusedLocationProviderClient(this);
         createLocationRequest();
-
-        if (savedInstanceState != null) {
-            Log.d(TAG, "Localization was : " + savedInstanceState.getBoolean("LocalizationSwitchState"));
-
-            this.ls.setChecked(savedInstanceState.getBoolean("LocalizationSwitchState"));
-        } else {
-            Log.d(TAG, "onCreate: savedInstanceState was null");
-        }
     }
 
-//    @Override
-//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-//        super.onRestoreInstanceState(savedInstanceState);
-//
-//        this.ls.setChecked(savedInstanceState.getBoolean("LocalizationSwitchState"));
-//    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        // same the localization switch state
-        Log.d(TAG, "Localization is : " + this.isLocalizationActive);
-        outState.putBoolean("LocalizationSwitchState", this.isLocalizationActive);
-    }
-
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        PreferenceManager.getDefaultSharedPreferences(this)
-//                .registerOnSharedPreferenceChangeListener(this);
-//    }
-
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        //updateButtonsState(Utils.getRequestingLocationUpdates(this));
-//        //mLocationUpdatesResultView.setText(Utils.getLocationUpdatesResult(this));
-//        client.flushLocations().addOnCompleteListener(new OnCompleteListener<Void>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Void> task) {
-//                if (!task.isSuccessful()) {
-//                    Log.d(TAG, "Flushing the client failed");
-//                }
-//            }
-//        });
-//    }
-
-//    @Override
-//    protected void onStop() {
-//        PreferenceManager.getDefaultSharedPreferences(this)
-//                .unregisterOnSharedPreferenceChangeListener(this);
-//        super.onStop();
-//    }
-
-    /**
-     * Sets up the location request. Android has two location request settings:
-     * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
-     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
-     * the AndroidManifest.xml.
-     * <p/>
-     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
-     * interval (5 seconds), the Fused Location Provider API returns location updates that are
-     * accurate to within a few feet.
-     * <p/>
-     * These settings are appropriate for mapping applications that show real-time location
-     * updates.
-     */
     private void createLocationRequest() {
         lr = new LocationRequest();
 
-        // Sets the desired interval for active location updates. This interval is
-        // inexact. You may not receive updates at all if no location sources are available, or
-        // you may receive them slower than requested. You may also receive updates faster than
-        // requested if other applications are requesting location at a faster interval.
-        // Note: apps running on "O" devices (regardless of targetSdkVersion) may receive updates
-        // less frequently than this interval when the app is no longer in the foreground.
         lr.setInterval(UPDATE_INTERVAL);
-
-        // Sets the fastest rate for active location updates. This interval is exact, and your
-        // application will never receive updates faster than this value.
         lr.setFastestInterval(FASTEST_UPDATE_INTERVAL);
-
         lr.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // Sets the maximum time when batched location updates are delivered. Updates may be
-        // delivered sooner than this interval.
         lr.setMaxWaitTime(MAX_WAIT_TIME);
     }
 
     private PendingIntent getPendingIntent() {
-        // Note: for apps targeting API level 25 ("Nougat") or lower, either
-        // PendingIntent.getService() or PendingIntent.getBroadcast() may be used when requesting
-        // location updates. For apps targeting API level O, only
-        // PendingIntent.getBroadcast() should be used. This is due to the limits placed on services
-        // started in the background in "O".
-
-        // TODO(developer): uncomment to use PendingIntent.getService().
         Intent intent = new Intent(this, LocalizationService.class);
         intent.setAction(LocalizationService.ACTION_PROCESS_UPDATES);
         return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-//        Intent intent = new Intent(this, LocationUpdatesBroadcastReceiver.class);
-//        intent.setAction(LocationUpdatesBroadcastReceiver.ACTION_PROCESS_UPDATES);
-//        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /**
-     * Return the current state of the permissions needed.
+     * Check the permission required for the localization service
+     * @return true if the application has the required permission
      */
     private boolean checkPermissions() {
         int fineLocationPermissionState = ActivityCompat.checkSelfPermission(
@@ -212,52 +106,15 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
                 (backgroundLocationPermissionState == PackageManager.PERMISSION_GRANTED);
     }
 
+    /**
+     * Request the localization permission
+     */
     private void requestPermissions() {
-
-        boolean permissionAccessFineLocationApproved =
-                ActivityCompat.checkSelfPermission(
-                        this, Manifest.permission.ACCESS_FINE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED;
-
-        boolean backgroundLocationPermissionApproved =
-                ActivityCompat.checkSelfPermission(
-                        this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED;
-
-        boolean shouldProvideRationale =
-                permissionAccessFineLocationApproved && backgroundLocationPermissionApproved;
-
-        // Provide an additional rationale to the user. This would happen if the user denied the
-        // request previously, but didn't check the "Don't ask again" checkbox.
-//        if (shouldProvideRationale) {
-//            Log.i(TAG, "Displaying permission rationale to provide additional context.");
-//            Snackbar.make(
-//                    findViewById(R.id.activity_main),
-//                    R.string.permission_rationale,
-//                    Snackbar.LENGTH_INDEFINITE)
-//                    .setAction(R.string.ok, new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View view) {
-//                            // Request permission
-//                            ActivityCompat.requestPermissions(MainActivity.this,
-//                                    new String[] {
-//                                            Manifest.permission.ACCESS_FINE_LOCATION,
-//                                            Manifest.permission.ACCESS_BACKGROUND_LOCATION },
-//                                    REQUEST_PERMISSIONS_REQUEST_CODE);
-//                        }
-//                    })
-//                    .show();
-//        } else {
-            Log.i(TAG, "Requesting permission");
-            // Request permission. It's possible this can be auto answered if device policy
-            // sets the permission in a given state or the user denied the permission
-            // previously and checked "Never ask again".
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[] {
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_BACKGROUND_LOCATION },
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
-//        }
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION },
+                REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
     /**
@@ -272,57 +129,16 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
                 // If user interaction was interrupted, the permission request is cancelled and you
                 // receive empty arrays.
                 Log.i(TAG, "User interaction was cancelled.");
-
+                this.ls.setClickable(false);
             } else if ((grantResults[0] == PackageManager.PERMISSION_GRANTED) &&
                     (grantResults[1] == PackageManager.PERMISSION_GRANTED)
             ) {
                 // Permission was granted.
-                requestLocationUpdates();
-
-            } else {
-                // Permission denied.
-                Log.d(TAG, "User denied permission");
-
-                // Notify the user via a SnackBar that they have rejected a core permission for the
-                // app, which makes the Activity useless. In a real app, core permissions would
-                // typically be best requested during a welcome-screen flow.
-
-                // Additionally, it is important to remember that a permission might have been
-                // rejected without asking the user for permission (device policy or "Never ask
-                // again" prompts). Therefore, a user interface affordance is typically implemented
-                // when permissions are denied. Otherwise, your app could appear unresponsive to
-                // touches or interactions which have required permissions.
-//                Snackbar.make(
-//                        findViewById(R.id.activity_main),
-//                        R.string.permission_denied_explanation,
-//                        Snackbar.LENGTH_INDEFINITE)
-//                        .setAction(R.string.settings, new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View view) {
-//                                // Build intent that displays the App settings screen.
-//                                Intent intent = new Intent();
-//                                intent.setAction(
-//                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-//                                Uri uri = Uri.fromParts("package",
-//                                        BuildConfig.APPLICATION_ID, null);
-//                                intent.setData(uri);
-//                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                startActivity(intent);
-//                            }
-//                        })
-//                        .show();
+                Log.d(TAG, "onRequestPermissionsResult: Permission granted");
+                this.ls.setClickable(true);
             }
         }
     }
-
-//    @Override
-//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-//        if (s.equals(Utils.KEY_LOCATION_UPDATES_RESULT)) {
-//            mLocationUpdatesResultView.setText(Utils.getLocationUpdatesResult(this));
-//        } else if (s.equals(Utils.KEY_LOCATION_UPDATES_REQUESTED)) {
-//            updateButtonsState(Utils.getRequestingLocationUpdates(this));
-//        }
-//    }
 
     /**
      * Handles the Request Updates button and requests start of location updates.
@@ -330,11 +146,9 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
     public void requestLocationUpdates() {
         try {
             Log.i(TAG, "Starting location updates");
-            //Utils.setRequestingLocationUpdates(this, true);
             client.requestLocationUpdates(lr, getPendingIntent());
             this.isLocalizationActive = true;
         } catch (SecurityException e) {
-            //Utils.setRequestingLocationUpdates(this, false);
             e.printStackTrace();
         }
     }
@@ -344,25 +158,9 @@ public class MainActivity extends AppCompatActivity { //implements SharedPrefere
      */
     public void removeLocationUpdates() {
         Log.i(TAG, "Removing location updates");
-        //Utils.setRequestingLocationUpdates(this, false);
         client.removeLocationUpdates(getPendingIntent());
         this.isLocalizationActive = false;
     }
-
-    /**
-     * Ensures that only one button is enabled at any time. The Start Updates button is enabled
-     * if the user is not requesting location updates. The Stop Updates button is enabled if the
-     * user is requesting location updates.
-     */
-//    private void updateButtonsState(boolean requestingLocationUpdates) {
-//        if (requestingLocationUpdates) {
-//            mRequestUpdatesButton.setEnabled(false);
-//            mRemoveUpdatesButton.setEnabled(true);
-//        } else {
-//            mRequestUpdatesButton.setEnabled(true);
-//            mRemoveUpdatesButton.setEnabled(false);
-//        }
-//    }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
--- app/src/main/java/ch/epfl/balelecbud/localization/LocalizationService.java
+++ app/src/main/java/ch/epfl/balelecbud/localization/LocalizationService.java
@@ -21,109 +21,10 @@ public class LocalizationService extends IntentService {
             "ch.epfl.balelecbud.localization.LocalizationService.PROCESS_UPDATES";
 
     private static final String TAG = LocalizationService.class.getSimpleName();
-    private static Activity activity;
-//    public static final int LOCATION_REQUEST_CODE = "ch.epfl.balelecbud.localization.localizationService.REQUEST".hashCode();
-//    public static final int PERMISSION_REQUEST_CODE = 100;
-//    private static LocalizationService localizationService = null;
-
-//    private final LocationRequest lr;
-//    private final LocationSettingsRequest lsr;
-//    private final PendingIntent pi;
-//    private final FusedLocationProviderClient client;
-//    private Location lastLocation = null;
-
-//    public static void getLocalizationService(Activity activity) {
-//
-//        Log.d(TAG, "Before Constructor");
-//        LocalizationService.activity = activity;
-//        if (localizationService == null)
-//            localizationService = new LocalizationService(activity);
-//        return localizationService;
-//    }
 
     public LocalizationService() {
         super(TAG);
-//        this.activity = activity;
-//        this.lr = LocationRequest.create()
-//            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-//            .setInterval(2 * 1000);
-//
-//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-//                .addLocationRequest(lr)
-//                .setAlwaysShow(false);
-//
-//        this.lsr = builder.build();
-//
-//
-////        Intent intent = new Intent(this, LocationUpdatesIntentService.class);
-////        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        Intent i = new Intent(activity, LocalizationService.class);
-//        i.setAction(LocalizationService.ACTION_PROCESS_UPDATES);
-//        this.pi = PendingIntent.getService(
-//                activity,
-//                LocalizationService.LOCATION_REQUEST_CODE,
-//                i, PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        this.client = LocationServices.getFusedLocationProviderClient(activity);
     }
-
-//    private void checkPermission() {
-//        Task<LocationSettingsResponse> result =
-//                LocationServices.getSettingsClient(this.activity).checkLocationSettings(this.lsr);
-//
-//        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
-//            @Override
-//            public void onComplete(Task<LocationSettingsResponse> task) {
-//                try {
-//                    LocationSettingsResponse response = task.getResult(ApiException.class);
-//                    // All location settings are satisfied. The client can initialize location
-//                    // requests here.
-//                    LocalizationService.this.initializeLocalization();
-//                } catch (ApiException exception) {
-//                    switch (exception.getStatusCode()) {
-//                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-//                            Toast.makeText(activity, "Location permission needed", Toast.LENGTH_SHORT).show();
-//                            // Location settings are not satisfied. But could be fixed by showing the
-//                            // user a dialog.
-//                            try {
-//                                // Cast to a resolvable exception.
-//                                ResolvableApiException resolvable = (ResolvableApiException) exception;
-//                                // Show the dialog by calling startResolutionForResult(),
-//                                // and check the result in onActivityResult().
-//                                resolvable.startResolutionForResult(
-//                                        activity,
-//                                        LocalizationService.PERMISSION_REQUEST_CODE);
-//                            } catch (IntentSender.SendIntentException e) {
-//                                // Ignore the error.
-//                            } catch (ClassCastException e) {
-//                                // Ignore, should be an impossible error.
-//                            }
-//                            break;
-//                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-//                            // Location settings are not satisfied. However, we have no way to fix the
-//                            // settings so we won't show the dialog.
-//                            Toast.makeText(LocalizationService.this, "Location unavailable", Toast.LENGTH_SHORT).show();
-//                            break;
-//                    }
-//                }
-//            }
-//        });
-//    }
-
-//    private void initializeLocalization() {
-//        Log.d("fghj", "coucou ici");
-//        client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Location> task) {
-//                if (task.isSuccessful()) {
-//                    displayLocation(task.getResult());
-//                } else {
-//                    Log.d("gvhbn", "lastLocation failed");
-//                }
-//            }
-//        });
-//        //client.requestLocationUpdates(lr, pi);
-//    }
 
     private void handleLocation(Location location) {
         if (location != null) {
@@ -138,43 +39,18 @@ public class LocalizationService extends IntentService {
                     Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
                 }
             });
-            //Toast.makeText(activity, String.format(Locale.US, "lat: %1$f, lon: %2$f", latitude, longitude), Toast.LENGTH_LONG).show();
-        } else {
-            Log.d(TAG, "Location not available");
-            //Toast.makeText(this.getApplicationContext(), "Location not availible", Toast.LENGTH_SHORT).show();
         }
     }
 
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        //checkPermission();
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        //client.removeLocationUpdates(this.pi);
-//    }
-
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.d(TAG, "coucou from IntentHandle");
         if (intent != null) {
-            Log.d(TAG, "intent non null");
             final String action = intent.getAction();
             if (ACTION_PROCESS_UPDATES.equals(action)) {
-                Log.d(TAG, "intent is for me ");
                 LocationResult result = LocationResult.extractResult(intent);
                 if (result != null) {
-                    Log.d(TAG, "Intent has a Location");
                     Location locations = result.getLastLocation();
                     handleLocation(locations);
-//                    Utils.setLocationUpdatesResult(this, locations);
-//                    Utils.sendNotification(this, Utils.getLocationResultTitle(this, locations));
-//                    Log.i(TAG, Utils.getLocationUpdatesResult(this));
-                } else {
-                    Log.d(TAG, "Intent has no Location :-(");
                 }
             }
         }
