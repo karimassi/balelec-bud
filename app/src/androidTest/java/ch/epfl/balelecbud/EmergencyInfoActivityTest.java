d0d5befc1dbeb17315264730e11883d3c3644037
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-03-12 19:13:03 +0100

    Add tests for EmergencyNumbers and EmergencyInfo

---
+++ app/src/androidTest/java/ch/epfl/balelecbud/EmergencyInfoActivityTest.java
@@ -0,0 +1,42 @@
+package ch.epfl.balelecbud;
+import android.view.View;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+@RunWith(AndroidJUnit4.class)
+public class EmergencyInfoActivityTest {
+    @Rule
+    public final ActivityTestRule<EmergencyInfoActivity> mActivityRule =
+            new ActivityTestRule<>(EmergencyInfoActivity.class);
+
+    @Test
+    public void testTextViewIsNonNull() {
+        EmergencyInfoActivity mActivity = mActivityRule.getActivity();
+        View viewById = mActivity.findViewById(R.id.ermergencyTextView);
+        assertThat(viewById, notNullValue());
+    }
+
+    @Test
+    public void testTextViewIsDisplayed() {
+        onView(withId(R.id.ermergencyTextView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRefreshButtonIsDisplayed() {
+        onView(withId((R.id.refreshButton))).check(matches(isDisplayed()));
+    }
+}
---
+++ app/src/androidTest/java/ch/epfl/balelecbud/EmergencyNumbersActivityTest.java
@@ -0,0 +1,43 @@
+package ch.epfl.balelecbud;
+
+import android.view.View;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+
+@RunWith(AndroidJUnit4.class)
+public class EmergencyNumbersActivityTest {
+    @Rule
+    public final ActivityTestRule<EmergencyNumbersActivity> mActivityRule =
+            new ActivityTestRule<>(EmergencyNumbersActivity.class);
+
+    @Test
+    public void testListViewIsNonNull() {
+        EmergencyNumbersActivity mActivity = mActivityRule.getActivity();
+        View viewById = mActivity.findViewById(R.id.numbersListView);
+        assertThat(viewById, notNullValue());
+    }
+
+    @Test
+    public void testListViewIsDisplayed() {
+        onView(withId(R.id.numbersListView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRefreshButtonIsDisplayed() {
+        onView(withId((R.id.refreshButton))).check(matches(isDisplayed()));
+    }
+}
+
