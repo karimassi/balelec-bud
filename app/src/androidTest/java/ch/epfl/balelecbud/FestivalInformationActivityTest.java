3997c45621eefa2597f6a8e990f70e6589500afb
celinecamacho <celine.camacho@epfl.ch>
2020-03-12 19:38:58 +0100

    Modified the AndroidManifest

--- app/src/androidTest/java/ch/epfl/balelecbud/FestivalInformationActivityTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/FestivalInformationActivityTest.java
@@ -1,9 +1,5 @@
 package ch.epfl.balelecbud;
 
-import android.view.View;
-
-import androidx.test.espresso.ViewAssertion;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -11,16 +7,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
+
+import ch.epfl.balelecbud.FestivalInformation.FestivalInformation;
+import ch.epfl.balelecbud.FestivalInformation.FestivalInformationListener;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 
 @RunWith(AndroidJUnit4.class)
 public class FestivalInformationActivityTest {
--- app/src/main/java/ch/epfl/balelecbud/FestivalInformation/FestivalInformationListener.java
+++ app/src/main/java/ch/epfl/balelecbud/FestivalInformation/FestivalInformationListener.java
@@ -4,27 +4,27 @@ import java.util.List;
 
 public class FestivalInformationListener {
 
-    private FestivalInformationAdapterFacade facade;
-    private List<FestivalInformation> informationList;
+    private static FestivalInformationAdapterFacade facade;
+    private static List<FestivalInformation> informationList;
 
     public FestivalInformationListener(FestivalInformationAdapterFacade facade, List<FestivalInformation> informationList) {
         this.facade = facade;
         this.informationList = informationList;
     }
 
-    public void addInformation(FestivalInformation information, int index) {
+    public static void addInformation(FestivalInformation information, int index) {
         informationList.add(index, information);
         facade.notifyItemInserted(index);
     }
 
-    public void modifyInformation(FestivalInformation information, int index) {
+    public static void modifyInformation(FestivalInformation information, int index) {
         if (index < informationList.size()) {
             informationList.set(index, information);
             facade.notifyItemModified(index);
         }
     }
 
-    public void removeInformation(FestivalInformation information, int index) {
+    public static void removeInformation(FestivalInformation information, int index) {
         if (informationList.contains(information)) {
             informationList.remove(information);
             facade.notifyItemRemoved(index);
