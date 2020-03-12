21e478e68204f83cdba13c830fb2a6d92cc9649e
Camille Montemagni <camillemontemagni@tsf-436-wpa-7-215.epfl.ch>
2020-03-12 16:08:11 +0100

    added info and transport buttons and tests

--- app/src/main/java/ch/epfl/balelecbud/WelcomeActivity.java
+++ app/src/main/java/ch/epfl/balelecbud/WelcomeActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 
 import ch.epfl.balelecbud.authentication.FirebaseAuthenticator;
+import ch.epfl.balelecbud.schedule.ScheduleActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -17,11 +20,43 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        Button infoButton = findViewById(R.id.infoButton);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfoActivity();
+            }
+        });
+
+        Button scheduleButton = findViewById(R.id.scheduleButton);
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScheduleActivity();
+            }
+        });
+
         Button mapButton = findViewById(R.id.mapButton);
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMapActivity();
+            }
+        });
+
+        Button transportButton = findViewById(R.id.transportButton);
+        transportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTransportActivity();
+            }
+        });
+
+        Switch locationSwitch = findViewById(R.id.locationSwitch);
+        locationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //should switch on the location
             }
         });
     }
@@ -43,5 +78,23 @@ public class WelcomeActivity extends AppCompatActivity {
     public void openMapActivity () {
         Intent intent = new Intent(this, MapViewActivity.class);
         startActivity(intent);
+    }
+
+    /** Called when the user clicks the Schedule button */
+    public void openScheduleActivity(){
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Schedule button */
+    public void openInfoActivity(){
+        //Intent intent = new Intent(this, ScheduleActivity.class);
+        //startActivity(intent);
+    }
+
+    /** Called when the user clicks the Schedule button */
+    public void openTransportActivity(){
+        //Intent intent = new Intent(this, ScheduleActivity.class);
+        //startActivity(intent);
     }
 }
