03be32ea9d0500ff98ed9c0852f50dbc8a709946
Axel Marmet <axel.marmet@epfl.ch>
2020-03-17 11:07:42 +0100

    Start feature/concert-notification

---
+++ app/src/main/java/ch/epfl/balelecbud/notifications/concertSoon/ConcertSoonNotificationScheduler.java
@@ -0,0 +1,10 @@
+package ch.epfl.balelecbud.notifications.concertSoon;
+
+import ch.epfl.balelecbud.schedule.models.Slot;
+
+public class ConcertSoonNotificationScheduler {
+
+    public void scheduleNotification(Slot slot){
+        //stub implementation
+    }
+}
