9bce7aa77e0ab4781aa6bb77ec8689db919d46d7
Axel <axel.marmet@epfl.ch>
2020-03-17 11:34:55 +0100

    Rename files and add interface to allow easier mocking in tests
    We decided to rename the files because the name were becoming ridiculously long and we decided that there was no need to be redundant in the names since the packages already contains all the information

--- app/src/main/java/ch/epfl/balelecbud/notifications/concertSoon/Scheduler.java
+++ app/src/main/java/ch/epfl/balelecbud/notifications/concertSoon/Scheduler.java
@@ -2,7 +2,7 @@ package ch.epfl.balelecbud.notifications.concertSoon;
 
 import ch.epfl.balelecbud.schedule.models.Slot;
 
-public class ConcertSoonNotificationScheduler {
+public class Scheduler implements  SchedulerInterface{
 
     public void scheduleNotification(Slot slot){
         //stub implementation
