cebef57150f03bf62883381642d60ebfa6163175
Camille Montemagni <camillemontemagni@MacBook-Pro-de-Camille.local>
2020-03-20 00:54:56 +0100

    added the subscribe switches

--- app/src/main/java/ch/epfl/balelecbud/schedule/ScheduleAdapter.java
+++ app/src/main/java/ch/epfl/balelecbud/schedule/ScheduleAdapter.java
@@ -1,9 +1,12 @@
 package ch.epfl.balelecbud.schedule;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,15 +16,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.balelecbud.R;
+import ch.epfl.balelecbud.notifications.concertDataBase.ConcertDatabaseInterface;
 import ch.epfl.balelecbud.schedule.models.Slot;
 import ch.epfl.balelecbud.util.adapters.RecyclerViewAdapterFacade;
 import ch.epfl.balelecbud.util.database.DatabaseListener;
 import ch.epfl.balelecbud.util.database.DatabaseWrapper;
 import ch.epfl.balelecbud.util.database.FirestoreDatabaseWrapper;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder> {
 
     private static DatabaseWrapper database = new FirestoreDatabaseWrapper();
+    private static ConcertDatabaseInterface notifDB;
 
     @VisibleForTesting
     public static void setDatabaseImplementation(DatabaseWrapper databaseWrapper){
@@ -34,12 +41,14 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
         public TextView timeSlotView;
         public TextView artistNameView;
         public TextView sceneNameView;
+        public Switch subscribeSwitch;
 
         public ScheduleViewHolder(View itemView) {
             super(itemView);
             timeSlotView = itemView.findViewById(R.id.time_slot);
             artistNameView = itemView.findViewById(R.id.artist_name);
             sceneNameView = itemView.findViewById(R.id.scene_name);
+            subscribeSwitch = itemView.findViewById(R.id.subscribe_switch);
         }
     }
 
@@ -77,10 +86,24 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
 
     @Override
     public void onBindViewHolder(ScheduleAdapter.ScheduleViewHolder viewHolder, int position) {
-        Slot slot = slots.get(position);
+        final Slot slot = slots.get(position);
         viewHolder.timeSlotView.setText(slot.getTimeSlot());
         viewHolder.artistNameView.setText(slot.getArtistName());
         viewHolder.sceneNameView.setText(slot.getSceneName());
+
+        /**subscribeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The location is enabled
+                    Log.d(TAG, "Location switched: ON");
+                    notifDB.scheduleNewConcert(slot);
+                } else {
+                    // The location is disabled
+                    Log.d(TAG,"Location switched: OFF");
+                    notifDB.removeConcert(slot);
+                }
+            }
+        });**/
     }
 
     public int getItemCount() {
