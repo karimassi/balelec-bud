43872d3bf3d3c5ed4746bef657528a8ce0978710
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-03-23 16:33:16 +0100

    Add model for point of interest

---
+++ app/src/main/java/ch/epfl/balelecbud/models/PointOfInterest.java
@@ -0,0 +1,38 @@
+package ch.epfl.balelecbud.models;
+
+import com.google.firebase.firestore.GeoPoint;
+
+public class PointOfInterest {
+    private String name;
+    private String type;
+    private String poiToken;
+    private GeoPoint location;
+
+
+    public PointOfInterest() {
+    }
+
+    public PointOfInterest(GeoPoint location, String name, String type, String poiToken) {
+        this.location = location;
+        this.name = name;
+        this.type = type;
+        this.poiToken = poiToken;
+    }
+
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPoiToken() {
+        return poiToken;
+    }
+}
