d7f7c7ddab12d31e2c28153b7cd537a5245375d9
Axel <axel.marmet@epfl.ch>
2020-03-27 17:27:35 +0100

    Add machinery to easily create RecyclerViewAdapters

---
+++ app/src/main/java/ch/epfl/balelecbud/util/views/RecyclerViewData.java
@@ -0,0 +1,36 @@
+package ch.epfl.balelecbud.util.views;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class RecyclerViewData <A, B extends RecyclerView.ViewHolder> {
+
+    protected List<A> data = new LinkedList<>();
+    private RecyclerView.Adapter<B> rvAdapter;
+
+    public void setAdapter(RecyclerView.Adapter<B> adapter){
+        this.rvAdapter = adapter;
+    }
+
+    public abstract void reload();
+
+    public abstract void bind(int index, B viewHolder);
+
+    public void add(int index, A newElem){
+        data.add(index, newElem);
+        rvAdapter.notifyItemInserted(index);
+    }
+
+    public void remove(int index){
+        data.remove(index);
+        rvAdapter.notifyItemRemoved(index);
+        rvAdapter.notifyItemRangeChanged(index, data.size());
+    }
+
+    public int size(){
+        return data.size();
+    }
+
+}
---
+++ app/src/main/java/ch/epfl/balelecbud/util/views/StandardRecyclerViewAdapter.java
@@ -0,0 +1,45 @@
+package ch.epfl.balelecbud.util.views;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StandardRecyclerViewAdapter<A, B extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<B> {
+
+    private ViewHolderFactory<B> factory;
+    private RecyclerViewData<A, B> data;
+    private int itemId;
+
+    public StandardRecyclerViewAdapter(ViewHolderFactory<B> factory, RecyclerViewData<A, B> data, int itemId) {
+        this.factory = factory;
+        this.data = data;
+        this.itemId = itemId;
+        data.setAdapter(this);
+        data.reload();
+    }
+
+    public void reloadData(){
+        data.reload();
+    }
+
+    @NonNull
+    @Override
+    public B onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(itemId, parent, false);
+        return factory.createInstance(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull B holder, int position) {
+        data.bind(position, holder);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+}
