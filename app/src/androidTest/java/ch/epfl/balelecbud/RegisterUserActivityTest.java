ebbafa3465cb965d5b958ccb78edd2a6f79d8d94
Axel <axel.marmet@epfl.ch>
2020-04-01 17:44:46 +0200

    Small fixes to different androidTests

--- app/src/androidTest/java/ch/epfl/balelecbud/RegisterUserActivityTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/RegisterUserActivityTest.java
@@ -216,7 +216,7 @@ public class RegisterUserActivityTest extends BasicAuthenticationTest {
             }
 
             @Override
-            public void deleteDocument(String collectionName, String documentID) {
+            public void deleteDocumentWithID(String collectionName, String documentID) {
 
             }
         });
