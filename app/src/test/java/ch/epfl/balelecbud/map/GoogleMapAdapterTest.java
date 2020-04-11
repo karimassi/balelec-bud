1004ef6c2e77f8f660ff50d91adae9f48ffd7770
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-04-11 23:27:41 +0200

    Adds dummy tests for GoogleMapAdapter

---
+++ app/src/test/java/ch/epfl/balelecbud/map/GoogleMapAdapterTest.java
@@ -0,0 +1,24 @@
+package ch.epfl.balelecbud.map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class GoogleMapAdapterTest {
+    @Test
+    public void googleMapAdapterWithNullMap() {
+        // check that no error is throw
+        new GoogleMapAdapter(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setMyLocationEnabledWithNullMapThrowsNPE() {
+        new GoogleMapAdapter(null).setMyLocationEnabled(false);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addMarkerWithNullMapThrowsNPE() {
+        new GoogleMapAdapter(null).addMarker(new MyMarker.Builder());
+    }
+}
