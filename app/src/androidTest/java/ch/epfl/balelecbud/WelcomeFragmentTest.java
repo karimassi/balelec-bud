27fad9a3aa7bed85ec5f7f5fe8a2942e55ba6306
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-04-29 22:59:18 +0200

    Fix location switch been in setting fragment now

--- app/src/androidTest/java/ch/epfl/balelecbud/WelcomeFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/WelcomeFragmentTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -14,9 +13,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class WelcomeFragmentTest extends RootActivityTest {
     @Test
-    public void testToggleLocationIsDisplayed() {
-        onView(withId(R.id.locationSwitch)).check(matches(isDisplayed()));
-        onView(withId(R.id.locationSwitch)).perform(click());
+    public void backGroundIsDisplayed() {
+        onView(withId(R.id.activity_home_linear_layout)).check(matches(isDisplayed()));
     }
 
     @Override
--- app/src/main/java/ch/epfl/balelecbud/WelcomeFragment.java
+++ app/src/main/java/ch/epfl/balelecbud/WelcomeFragment.java
@@ -1,22 +1,13 @@
 package ch.epfl.balelecbud;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-
-import ch.epfl.balelecbud.location.LocationUtil;
 
 public class WelcomeFragment extends Fragment {
-    private static final String TAG = RootActivity.class.getSimpleName();
-    private Switch locationSwitch;
-    private FragmentActivity activity;
 
     public static WelcomeFragment newInstance() {
         return (new WelcomeFragment());
@@ -26,60 +17,5 @@ public class WelcomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        activity = this.getActivity();
-        setUpLocation();
-    }
-
-    private void setUpLocation() {
-        this.locationSwitch = getView().findViewById(R.id.locationSwitch); // Not sure about this
-        this.locationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                Log.d(TAG, "Location switched: ON");
-                LocationUtil.enableLocation();
-            } else {
-                Log.d(TAG, "Location switched: OFF");
-                LocationUtil.disableLocation();
-            }
-        });
-        LocationUtil.requestLocationPermission(this);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == LocationUtil.LOCATION_PERMISSIONS_REQUEST_CODE) {
-            LocationUtil.onLocationRequestPermissionsResult(
-                    grantResults,
-                    onPermissionNotGranted("Permission request canceled"),
-                    onPermissionGranted(),
-                    onPermissionNotGranted("Permission denied")
-            );
-        }
-    }
-
-    private Runnable onPermissionNotGranted(final String logText) {
-        return () -> {
-            Log.i(TAG, "onRequestPermissionsResult: " + logText);
-            locationSwitch.setClickable(false);
-            activity.runOnUiThread(() ->
-                    locationSwitch.setChecked(false));
-        };
-    }
-
-    private Runnable onPermissionGranted() {
-        return () -> {
-            Log.i(TAG, "onRequestPermissionsResult: Permission granted");
-            locationSwitch.setClickable(true);
-            if (LocationUtil.isLocationActive()) {
-                Log.d(TAG, "onPermissionGranted: location was active");
-                activity.runOnUiThread(() ->
-                        locationSwitch.setChecked(true));
-            }
-        };
     }
 }
