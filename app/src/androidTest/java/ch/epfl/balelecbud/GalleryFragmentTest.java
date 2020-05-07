58df4de0e66dff909c9cfed3a329491f2b6bffad
Camille Montemagni <camille.montemagni@epfl.ch>
2020-05-07 15:57:11 +0200

    Added tests on items in the grid

--- app/src/androidTest/java/ch/epfl/balelecbud/GalleryFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/GalleryFragmentTest.java
@@ -7,8 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-
 import ch.epfl.balelecbud.gallery.GalleryFragment;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -16,11 +14,10 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static ch.epfl.balelecbud.testUtils.CustomMatcher.nthChildOf;
 
 @RunWith(AndroidJUnit4.class)
 public class GalleryFragmentTest {
-
-    private ArrayList<Integer> images;
 
     @Before
     public void setup() {
@@ -36,5 +33,18 @@ public class GalleryFragmentTest {
     @Test
     public void imagesAreDisplayed() {
         onView(withId(R.id.gallery_grid_view)).check(matches(hasChildCount(9)));
+        imageIsDisplayed(0);
+        imageIsDisplayed(1);
+        imageIsDisplayed(2);
+        imageIsDisplayed(3);
+        imageIsDisplayed(4);
+        imageIsDisplayed(5);
+        imageIsDisplayed(6);
+        imageIsDisplayed(7);
+        imageIsDisplayed(8);
+    }
+
+    private void imageIsDisplayed(int childPosition){
+        onView(nthChildOf(withId(R.id.gallery_grid_view), childPosition)).check(matches(isDisplayed()));
     }
 }
