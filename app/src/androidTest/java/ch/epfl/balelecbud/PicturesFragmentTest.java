d49beee3afbbaa1faefb614c1e6b1946ca62aa4c
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-05-08 01:20:49 +0200

    Update intermediary conversion from activity to fragment

--- app/src/androidTest/java/ch/epfl/balelecbud/PicturesFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/PicturesFragmentTest.java
@@ -4,64 +4,45 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Picture;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
-import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
-import androidx.test.uiautomator.UiSelector;
 
-import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import ch.epfl.balelecbud.authentication.Authenticator;
-import ch.epfl.balelecbud.authentication.MockAuthenticator;
-import ch.epfl.balelecbud.models.User;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.hamcrest.EasyMock2Matchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
-public class PicturesActivityTest extends BasicActivityTest {
+public class PicturesFragmentTest {
     private static UiDevice mDevice;
 
-    // IntentsTestRule is an extension of ActivityTestRule. IntentsTestRule sets up Espresso-Intents
-    // before each Test is executed to allow stubbing and validation of intents.
+
     @Rule
-    public IntentsTestRule<PicturesActivity> intentsRule = new IntentsTestRule<>(PicturesActivity.class);
+    public IntentsTestRule<FragmentActivity> intentsRule = new IntentsTestRule<>(PictureActivity.class);
+
     @Before
-    public void setUp() throws Exception{
+    public void setUp(){
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        FragmentScenario.launchInContainer(PicturesFragment.class);
     }
 
     @Test
@@ -118,17 +99,9 @@ public class PicturesActivityTest extends BasicActivityTest {
         return hasImage;
     }
 
-
-
-
     @Test
     public void buttonIsDisplayed() {
         onView(withId(R.id.takePicBtn)).check(matches(isDisplayed()));
     }
 
-    @Override
-    protected void setIds() {
-        setIds(R.id.pictures_activity_drawer_layout, R.id.pictures_activity_nav_view);
-
-    }
 }
--- app/src/main/java/ch/epfl/balelecbud/PicturesFragment.java
+++ app/src/main/java/ch/epfl/balelecbud/PicturesFragment.java
@@ -2,19 +2,16 @@ package ch.epfl.balelecbud;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.MimeTypeMap;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,42 +19,65 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
-public class PicturesActivity extends BasicActivity {
+public class PicturesFragment extends Fragment {
+
+    public static PicturesFragment newInstance() {
+        return (new PicturesFragment());
+    }
+
 
     private ImageView imageView;
     private Button cameraButton;
     private String currentPhotoPath;
+    private FragmentActivity activity;
+
 
     private static final int CAMERA_PERM_CODE = 101;
     private static final int CAMERA_REQUEST_CODE = 102;
 
+
+
+//    public void onCreateView(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_pictures);
+//        configureToolBar(R.id.pictures_activity_toolbar);
+//        configureDrawerLayout(R.id.pictures_activity_drawer_layout);
+//        configureNavigationView(R.id.pictures_activity_nav_view);
+//
+//        cameraButton = findViewById(R.id.takePicBtn);
+//        imageView = findViewById(R.id.picturesImageView);
+//
+//        cameraButton.setOnClickListener(v -> askCameraPermissions());
+//
+//    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pictures);
-        configureToolBar(R.id.pictures_activity_toolbar);
-        configureDrawerLayout(R.id.pictures_activity_drawer_layout);
-        configureNavigationView(R.id.pictures_activity_nav_view);
-
-        cameraButton = findViewById(R.id.takePicBtn);
-        imageView = findViewById(R.id.picturesImageView);
-
+    public void onStart() {
+        super.onStart();
+        activity = getActivity();
+        cameraButton = getActivity().findViewById(R.id.takePicBtn);
         cameraButton.setOnClickListener(v -> askCameraPermissions());
+    }
 
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        return inflater.inflate(R.layout.fragment_pictures, container, false);
     }
 
     private void askCameraPermissions() {
-        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
     }
 
     @Override
@@ -66,13 +86,13 @@ public class PicturesActivity extends BasicActivity {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 dispatchTakePictureIntent();
             }else {
-                Toast.makeText(this, R.string.camera_permission_refused_msg, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), R.string.camera_permission_refused_msg, Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
@@ -82,7 +102,7 @@ public class PicturesActivity extends BasicActivity {
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 Uri contentUri = Uri.fromFile(f);
                 mediaScanIntent.setData(contentUri);
-                this.sendBroadcast(mediaScanIntent);
+                getActivity().sendBroadcast(mediaScanIntent);
             }
         }
 
@@ -91,7 +111,7 @@ public class PicturesActivity extends BasicActivity {
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -103,7 +123,7 @@ public class PicturesActivity extends BasicActivity {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
@@ -111,7 +131,7 @@ public class PicturesActivity extends BasicActivity {
 
             }
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
                         "ch.epfl.balelecbud.android.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
@@ -119,5 +139,6 @@ public class PicturesActivity extends BasicActivity {
             }
         }
     }
+
 
 }
