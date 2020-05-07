14c9b5700e491c3a53b2a312e9f94d7623596ba3
Karim Assi <karim.assi@epfl.ch>
2020-05-07 19:39:09 +0200

    Fix flakiness and add more tests

--- app/src/androidTest/java/ch/epfl/balelecbud/util/database/CachedDatabaseTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/util/database/CachedDatabaseTest.java
@@ -5,14 +5,20 @@ import android.util.Log;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.firestore.FieldValue;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import ch.epfl.balelecbud.BalelecbudApplication;
@@ -21,67 +27,96 @@ import ch.epfl.balelecbud.models.Location;
 import ch.epfl.balelecbud.testUtils.TestAsyncUtils;
 import ch.epfl.balelecbud.util.cache.Cache;
 
-import static ch.epfl.balelecbud.BalelecbudApplication.getAppDatabase;
+import static ch.epfl.balelecbud.util.database.Database.DOCUMENT_ID_OPERAND;
 import static ch.epfl.balelecbud.util.database.Database.FESTIVAL_INFORMATION_PATH;
+import static ch.epfl.balelecbud.util.database.Database.FRIENDSHIPS_PATH;
+import static ch.epfl.balelecbud.util.database.Database.FRIEND_REQUESTS_PATH;
 import static ch.epfl.balelecbud.util.database.Database.LOCATIONS_PATH;
 import static ch.epfl.balelecbud.util.database.Database.Source.CACHE;
 import static ch.epfl.balelecbud.util.database.Database.Source.REMOTE;
+import static ch.epfl.balelecbud.util.database.Database.TOKENS_PATH;
+import static ch.epfl.balelecbud.util.database.MockDatabase.karim;
+import static ch.epfl.balelecbud.util.database.MyWhereClause.Operator.EQUAL;
 
 @RunWith(AndroidJUnit4.class)
 public class CachedDatabaseTest {
 
-    private Cache appCache;
-    private MockDatabase mockDatabase;
+    private MockDatabase remoteDatabase;
+    private CachedDatabase appDatabase;
+    Cache appCache;
 
-    FestivalInformation freshInfo = new FestivalInformation("Information", "Fresh information");
-    FestivalInformation staleInfo = new FestivalInformation("Information", "Stale information");
+    private FestivalInformation freshInfo = new FestivalInformation("Information", "Fresh information");
+    private FestivalInformation staleInfo = new FestivalInformation("Information", "Stale information");
+    private Map<String, Object> freshFriend = new HashMap<>();
+    private Map<String, Object> staleFriend = new HashMap<>();
 
     @Before
     public void setup() throws IOException {
-        mockDatabase = MockDatabase.getInstance();
-        mockDatabase.resetDatabase();
+        remoteDatabase = MockDatabase.getInstance();
+        appDatabase = CachedDatabase.getInstance();
         appCache = BalelecbudApplication.getAppCache();
+        BalelecbudApplication.setRemoteDatabase(remoteDatabase);
+        BalelecbudApplication.setAppDatabase(appDatabase);
+
+        freshFriend.put("11", true);
+        staleFriend.put("10", true);
+
+        remoteDatabase.resetDatabase();
+        remoteDatabase.storeDocumentWithID(FESTIVAL_INFORMATION_PATH, "information", freshInfo);
+        remoteDatabase.storeDocumentWithID(FRIENDSHIPS_PATH, karim.getUid(), freshFriend);
+
         appCache.flush();
-        BalelecbudApplication.setRemoteDatabase(mockDatabase);
-        mockDatabase.storeDocumentWithID(FESTIVAL_INFORMATION_PATH, "information", freshInfo);
         appCache.put(FESTIVAL_INFORMATION_PATH, "information", staleInfo);
+        appCache.put(FRIENDSHIPS_PATH, karim.getUid(), staleFriend);
     }
 
     @Test
-    public void getDocumentForceRemote() throws Throwable {
+    public void getDocumentsForceRemote() throws Throwable {
         MyQuery query = new MyQuery(FESTIVAL_INFORMATION_PATH, Collections.emptyList(), REMOTE);
         assertResults(Collections.singletonList(freshInfo),
-                getAppDatabase().query(query, FestivalInformation.class));
+                appDatabase.query(query, FestivalInformation.class));
+
+        query = new MyQuery(FRIENDSHIPS_PATH, new MyWhereClause(DOCUMENT_ID_OPERAND, EQUAL, karim.getUid()), REMOTE);
+        assertMapResults(Collections.singletonList(freshFriend),
+                appDatabase.query(query));
     }
 
-//    @Test
-//    public void getDocumentForceCache() throws Throwable {
-//        MyQuery query = new MyQuery(FESTIVAL_INFORMATION_PATH, Collections.emptyList(), CACHE);
-//        assertResults(Collections.singletonList(staleInfo),
-//                getAppDatabase().query(query, FestivalInformation.class));
-//    }
+    @Test
+    public void getDocumentForceCache() throws Throwable {
+        MyQuery query = new MyQuery(FESTIVAL_INFORMATION_PATH, Collections.emptyList(), CACHE);
+        assertResults(Collections.singletonList(staleInfo),
+                appDatabase.query(query, FestivalInformation.class));
+
+        query = new MyQuery(FRIENDSHIPS_PATH, new MyWhereClause(DOCUMENT_ID_OPERAND, EQUAL, karim.getUid()), CACHE);
+        assertMapResults(Collections.singletonList(staleFriend),
+                appDatabase.query(query));
+    }
 
     @Test
     public void allOperationsExceptQueryGoToRemote() throws Throwable{
-        Log.d("HERE", getAppDatabase().getClass().getSimpleName());
-        getAppDatabase().listenDocument(LOCATIONS_PATH, "0", location -> {}, Location.class);
-        Assert.assertEquals(1, MockDatabase.getInstance().getFriendsLocationListenerCount());
-        getAppDatabase().unregisterDocumentListener(LOCATIONS_PATH, "0");
-        Assert.assertEquals(0, MockDatabase.getInstance().getFriendsLocationListenerCount());
+        appDatabase.listenDocument(LOCATIONS_PATH, "0", location -> {}, Location.class);
+        Assert.assertEquals(1, remoteDatabase.getFriendsLocationListenerCount());
+        appDatabase.unregisterDocumentListener(LOCATIONS_PATH, "0");
+        Assert.assertEquals(0, remoteDatabase.getFriendsLocationListenerCount());
 
         MyQuery query = new MyQuery(LOCATIONS_PATH, Collections.emptyList());
 
-        getAppDatabase().storeDocumentWithID(LOCATIONS_PATH, "defaultLocation" ,Location.DEFAULT_LOCATION);
+        appDatabase.storeDocumentWithID(LOCATIONS_PATH, "defaultLocation" ,Location.DEFAULT_LOCATION);
         assertResults(Collections.singletonList(Location.DEFAULT_LOCATION),
-                mockDatabase.query(query, Location.class));
+                remoteDatabase.query(query, Location.class));
 
-        getAppDatabase().deleteDocumentWithID(LOCATIONS_PATH, "defaultLocation");
+        appDatabase.deleteDocumentWithID(LOCATIONS_PATH, "defaultLocation");
         assertResults(Collections.emptyList(),
-                mockDatabase.query(query, Location.class));
+                remoteDatabase.query(query, Location.class));
 
-        getAppDatabase().storeDocument(LOCATIONS_PATH, Location.DEFAULT_LOCATION);
+        appDatabase.storeDocument(LOCATIONS_PATH, Location.DEFAULT_LOCATION);
         assertResults(Collections.singletonList(Location.DEFAULT_LOCATION),
-                mockDatabase.query(query, Location.class));
+                remoteDatabase.query(query, Location.class));
+
+        query = new MyQuery(FRIEND_REQUESTS_PATH, new MyWhereClause(DOCUMENT_ID_OPERAND, EQUAL, karim.getUid()));
+        appDatabase.storeDocumentWithID(FRIEND_REQUESTS_PATH, karim.getUid(), new HashMap<String, Boolean>() {{ put("friend", true); }});
+        appDatabase.updateDocument(FRIEND_REQUESTS_PATH, karim.getUid(), new HashMap<String, Object>() {{ put("friend", FieldValue.delete()); }});
+        assertResults(Collections.singletonList(Collections.emptyMap()), appDatabase.query(query));
 
     }
 
@@ -89,6 +124,25 @@ public class CachedDatabaseTest {
         TestAsyncUtils sync = new TestAsyncUtils();
         actual.whenComplete((list, throwable) -> {
             if (throwable == null) {
+                sync.assertEquals(expected, list);
+                sync.call();
+            } else {
+                sync.fail();
+            }
+        });
+        sync.waitCall(1);
+        sync.assertCalled(1);
+        sync.assertNoFailedTests();
+    }
+
+    private void assertMapResults(List<Map<String, Object>> expected,
+                                  CompletableFuture<List<Map<String, Object>>> actual) throws Throwable{
+
+        TestAsyncUtils sync = new TestAsyncUtils();
+        actual.whenComplete((list, throwable) -> {
+            if (throwable == null) {
+                Log.d("TEST", "assertResults: " + expected.toString());
+                Log.d("TEST", "assertResults: " + list.toString());
                 sync.assertEquals(expected, list);
                 sync.call();
             } else {
