f9b45e009f7c02be42a298038efb60177009b980
celinecamacho <celine.camacho@epfl.ch>
2020-05-07 22:18:32 +0200

    Modify imports in WelcomeFragmentTest

--- app/src/androidTest/java/ch/epfl/balelecbud/WelcomeFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/WelcomeFragmentTest.java
@@ -1,7 +1,6 @@
 package ch.epfl.balelecbud;
 
 import androidx.fragment.app.testing.FragmentScenario;
-import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
@@ -11,14 +10,9 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.TestCase.assertNull;
-import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class WelcomeFragmentTest {
