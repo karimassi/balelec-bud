da7cf7035d74e9d3408f180005c9af366eb2725b
Karim Assi <karim.assi@epfl.ch>
2020-05-13 19:27:38 +0200

    Add more testS

--- app/src/androidTest/java/ch/epfl/balelecbud/view/emergency/SubmitEmergencyFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/view/emergency/SubmitEmergencyFragmentTest.java
@@ -19,19 +19,26 @@ import ch.epfl.balelecbud.utility.database.MockDatabase;
 import ch.epfl.balelecbud.utility.database.query.MyQuery;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.balelecbud.utility.database.MockDatabase.karim;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 @RunWith(AndroidJUnit4.class)
-public class EmergencyFragmentTest {
+public class SubmitEmergencyFragmentTest {
 
+    private FragmentScenario<SubmitEmergencyFragment> scenario;
     private final MockDatabase mockDB = MockDatabase.getInstance();
     private final MockAuthenticator mockAuth = MockAuthenticator.getInstance();
 
@@ -42,7 +49,7 @@ public class EmergencyFragmentTest {
         mockAuth.setCurrentUser(karim);
         BalelecbudApplication.setAppDatabase(mockDB);
         BalelecbudApplication.setAppAuthenticator(mockAuth);
-        FragmentScenario.launch(SubmitEmergencyFragment.class, null, R.style.Theme_AppCompat, null);
+        scenario = FragmentScenario.launch(SubmitEmergencyFragment.class, null, R.style.Theme_AppCompat, null);
     }
 
     @Test
@@ -62,6 +69,24 @@ public class EmergencyFragmentTest {
         });
         sync.waitCall(1);
         sync.assertNoFailedTests();
+    }
+
+    @Test
+    public void submitEmergencyEmptyMessage() {
+        submitEmergency("Theft", "");
+        onView(withText(R.string.emergency_ask_for_help)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void cancelDismissesFragment() {
+        scenario.onFragment(fragment -> {
+            assertThat(fragment.getDialog(), is(notNullValue()));
+            assertThat(fragment.requireDialog().isShowing(), is(true));
+            fragment.dismiss();
+            fragment.getParentFragmentManager().executePendingTransactions();
+            assertThat(fragment.getDialog(), is(nullValue()));
+        });
+
     }
 
 
