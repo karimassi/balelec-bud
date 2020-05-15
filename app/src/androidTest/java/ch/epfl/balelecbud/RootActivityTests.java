2fac4dfd2cf7201b8ea9de0f0d5fbcf61605d2af
Camille Montemagni <camille.montemagni@epfl.ch>
2020-05-15 09:48:32 +0200

    Added tests in RootActivity

--- app/src/androidTest/java/ch/epfl/balelecbud/RootActivityTests.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/RootActivityTests.java
@@ -89,6 +89,12 @@ public class RootActivityTests {
     }
 
     @Test
+    public void canOpenGalleryActivityFromDrawer(){
+        openDrawer();
+        clickItem(R.id.activity_main_drawer_gallery, R.id.galleryRecyclerView);
+    }
+
+    @Test
     public void canOpenTransportActivityFromDrawer() {
         openDrawer();
         clickItem(R.id.activity_main_drawer_transport, R.id.transport_fragment_container);
