ee2d2de6facde4be305659751cc76d2a14c796ff
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-05-17 08:29:11 +0200

    Add comments to file storage

--- app/src/main/java/ch/epfl/balelecbud/utility/storage/Storage.java
+++ app/src/main/java/ch/epfl/balelecbud/utility/storage/Storage.java
@@ -3,6 +3,9 @@ package ch.epfl.balelecbud.utility.storage;
 import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Interface modeling a file storage
+ */
 public interface Storage {
 
     CompletableFuture<File> getFile(String path);
