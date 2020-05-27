97d79e80859a9d1673e53110713bcd4ec6e9de99
Karim Assi <karim.assi@epfl.ch>
2020-05-27 16:33:30 +0200

    Removed unused imports

--- app/src/main/java/ch/epfl/balelecbud/view/friendship/SentRequestFragment.java
+++ app/src/main/java/ch/epfl/balelecbud/view/friendship/SentRequestFragment.java
@@ -2,7 +2,6 @@ package ch.epfl.balelecbud.view.friendship;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
