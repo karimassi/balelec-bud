89e14c31fe70f64be0795dda5596a3f5ff8234b0
celinecamacho <celine.camacho@epfl.ch>
2020-06-03 14:06:54 +0200

    Add import for ignore

--- app/src/androidTest/java/ch/epfl/balelecbud/view/GalleryFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/balelecbud/view/GalleryFragmentTest.java
@@ -14,6 +14,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
